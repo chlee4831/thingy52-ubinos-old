93ca03209d21666cbce52742ee0c142019f742cd
Sungho Park <siminsungho@gmail.com>
2020-03-22 18:58:39 +0900

    Apply version "ubinos-v03.00.09".

--- app/ex01_nrf52dk.cmake
+++ app/ex01_nrf52dk.cmake
@@ -1,15 +1,17 @@
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52dk.cmake)
 
+####
+
 set(INCLUDE__APP TRUE)
 set(APP__NAME "ex01")
 
-get_filename_component(_tmpdir "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
+get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
 
-file(GLOB_RECURSE _tmpsrc
-    "${_tmpdir}/*.c"
-    "${_tmpdir}/*.cpp"
-    "${_tmpdir}/*.S"
-    "${_tmpdir}/*.s")
+file(GLOB_RECURSE _tmp_sources
+    "${_tmp_source_dir}/*.c"
+    "${_tmp_source_dir}/*.cpp"
+    "${_tmp_source_dir}/*.S"
+    "${_tmp_source_dir}/*.s")
 
-set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmpsrc})
+set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
 
--- make/batch.mk
+++ make/batch.mk
@@ -4,7 +4,6 @@
 	@echo ""
 	@echo ""
 
-#######################################
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
@@ -12,6 +11,7 @@
 	make -f makefile.mk $@ CONFIG_NAME=ex01_nrf52dk
 	@echo ""
 	@echo ""
+
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
@@ -25,8 +25,25 @@
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_nrf52dk
 	@echo ""
 	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_nrf52840dk
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_stm3221geval
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_nucleof207zg
+	@echo ""
+	@echo ""
 
-#######################################
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
